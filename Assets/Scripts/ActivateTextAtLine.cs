db04954ba415f8a108a912632d34fbcf01e8d122
Eric Hornby <eric_hermit@hotmail.com>
2016-03-23 15:58:06 -0500

    Initial Asset Commit

---
+++ Assets/Scripts/ActivateTextAtLine.cs
@@ -0,0 +1,128 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ActivateTextAtLine : MonoBehaviour, IPointerClickHandler {
+
+    public TextAsset[] lookyTexts;
+    public int startLine;
+    public int endLine;
+    public int lookyClicks;
+    public TextBoxManager theTextBox;
+
+    public bool isItem;
+    Item item;
+
+    Interactable inter;
+    
+
+    public void OnPointerClick (PointerEventData eventData)
+    {
+        if (GameMaster.instance.actionState == GameMaster.ActionState.Looky)
+        {
+            Looky();
+        }
+        else if (GameMaster.instance.actionState == GameMaster.ActionState.Gimme)
+        {
+            Gimme();
+        }
+        else if (GameMaster.instance.actionState == GameMaster.ActionState.Insult)
+        {
+            Insult();
+        }
+        else if (GameMaster.instance.actionState == GameMaster.ActionState.Use)
+        {
+            Use();
+        }
+    }
+
+    void Looky() {
+        Debug.Log("Looky!");
+        if (!theTextBox.isActive || (theTextBox.isActive && theTextBox.itemMode))
+        {
+            theTextBox.DisableTextBox();
+            theTextBox.EnableTextBox();
+            theTextBox.ReloadScript(lookyTexts[lookyClicks]);
+            lookyClicks++;
+        }
+
+        if (lookyClicks >= lookyTexts.Length)
+            lookyClicks = 0;
+    }
+
+    void Gimme()
+    {
+        Debug.Log("gimme!");
+    }
+
+    void Insult()
+    {
+        Debug.Log("insult!");
+
+    }
+
+    void Use()
+    {
+        Debug.Log("use!");
+ 
+    }
+
+
+    public void OnMouseUp()
+    {
+        /*
+        if (GameMaster.instance.actionState == GameMaster.ActionState.Looky)
+        {
+            LookyText();
+        }
+        else if (GameMaster.instance.actionState == GameMaster.ActionState.Gimme)
+        {
+            GimmeText();
+        }
+        else if (GameMaster.instance.actionState == GameMaster.ActionState.Insult)
+        {
+            InsultText();
+        }
+        else if (GameMaster.instance.actionState == GameMaster.ActionState.Use)
+        {
+            UseTexts();
+        }
+        */
+    }
+
+    void Start() {
+        theTextBox = TextBoxManager.instance;
+
+        item = GetComponent<Item>();
+        if (item == null)
+        {
+            isItem = false;
+        }
+        else
+        {
+            isItem = true;
+        }
+
+        
+    }
+
+    public Item GetItem()
+    {
+        return item;
+    }
+
+    void Update() {
+
+    }
+
+    /*  
+    void OnTriggerEnter2D(Collider2D other)
+    { if (other.name == "Player")
+        { theTextBox.ReloadScript(theText);
+            theTextBox.currentLine = startLine;
+            theTextBox.endAtLine = endLine;
+
+        }
+    }
+    */
+}
---
+++ Assets/Scripts/MainPortrait.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class MainPortrait : MonoBehaviour {
+
+    public static MainPortrait instance;
+    public Image image;
+
+    public Sprite[] tippyFaces;
+
+    public void Neutral(string characterName)
+    {
+        if (characterName == "T")
+        {
+            image.sprite = tippyFaces[0];
+        }
+    }
+
+    public void Smile(string characterName)
+    {
+        if (characterName == "T")
+        {
+            image.sprite = tippyFaces[1];
+        }
+    }
+
+    public void Shocked(string characterName)
+    {
+        if (characterName == "T")
+        {
+            image.sprite = tippyFaces[2];
+        }
+    }
+
+    public void Angry(string characterName)
+    {
+        if (characterName == "T")
+        {
+            image.sprite = tippyFaces[3];
+        }
+    }
+
+    public void Sly(string characterName)
+    {
+        if (characterName == "T")
+        {
+            image.sprite = tippyFaces[4];
+        }
+    }
+
+    public void ChangePortrait(string characterName, string emotion, string effect)
+    {
+        Debug.Log("recieving Portrait Command: " + characterName + " " + emotion + " " + effect);
+
+        if (emotion == "")
+        {
+            Smile(characterName);
+        }
+        if (emotion == "Angry")
+        {
+            Angry(characterName);
+        }
+        if (emotion == "Shocked")
+        {
+            Shocked(characterName);
+        }
+        if (emotion == "Sly")
+        {
+            Sly(characterName);
+        }
+    }
+
+    void Awake () { instance = this; }
+
+	// Use this for initialization
+	void Start () {
+        image = this.GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
